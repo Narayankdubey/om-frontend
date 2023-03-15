8b5feb43bf0622f2abd5cba40c5335efe042cea2
Abhiram-vb <paidimarri.a@valuebound.com>
2023-03-15 21:11:33 +0530

    login,logout,input completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
+import { Fragment } from "react";
 
 import "./App.css";
 import { AnimatedRoutes } from "./components";
+import Notification from "./components/Notification/Notification";
+import { useSelector } from "react-redux";
 
 function App() {
+  const notification = useSelector((state) => state.ui.notification);
+
   return (
-    <BrowserRouter>
-      <AnimatedRoutes />
-    </BrowserRouter>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <BrowserRouter>
+        <AnimatedRoutes />
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
